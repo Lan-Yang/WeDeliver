6ddb20e489a5ebaf37e8c5a55d0d5d4945513a39
airwindow <350609633@qq.com>
2015-04-11 13:42:42 -0400

    update`

--- theApp/__init__.py
+++ theApp/__init__.py
@@ -5,7 +5,7 @@ The flask application package.
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
-from flask.ext.uploads import UploadSet, IMAGES, configure_uploads
+#from flask.ext.uploads import UploadSet, IMAGES, configure_uploads
 
 
 app = Flask(__name__)
@@ -16,8 +16,8 @@ db = SQLAlchemy(app)
 
 lm = LoginManager(app)
 
-photos = UploadSet('photos', IMAGES)
-configure_uploads(app, (photos,))
+#photos = UploadSet('photos', IMAGES)
+#configure_uploads(app, (photos,))
 
 # import FlaskWebProject.apis
 import theApp.views
