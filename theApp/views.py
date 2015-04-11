bd3232de5fe48001392de7e853472dc02189ffe5
Meng Wang <wm19922009@gmail.com>
2015-04-11 13:40:40 -0400

    reset db

--- theApp/views.py
+++ theApp/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 """
 from datetime import datetime
 from flask import render_template, request, flash, redirect, session, url_for, jsonify, send_file
-from theApp import app, db, lm, photos
+from theApp import app, db, lm, photos, controllers
 from flask.ext.login import login_required, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from jinja2 import Environment, Undefined
@@ -19,3 +19,8 @@ def init():
 def home():
     """Renders the home page."""
     return "hello world!"
+
+@app.route('/reset/db')
+def reset_db():
+	controllers.resetdb()
+	return "reset db."
