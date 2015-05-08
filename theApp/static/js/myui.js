2f9fbdf7331c73ab1dab5434ca25c37c1199cbb8
Meng Wang <wm19922009@gmail.com>
2015-05-07 23:46:08 -0400

    search result

---
+++ theApp/static/js/myui.js
@@ -0,0 +1,17 @@
+$(function () {
+  /* Date Picker */
+  $('.date').datepicker({
+    format: "mm/dd/yyyy"
+  });
+  /* Google Place Search */
+  // var autoComplete = [];
+  $('.place').each(function(index){
+    // console.log(this);
+    var ap = new google.maps.places.Autocomplete(this, {types: ['geocode']});
+    google.maps.event.addListener(ap, 'place_changed', function() {
+      var place = ap.getPlace();
+      console.log(place);
+    });
+    // autoComplete.push(ap);
+  });
+});
